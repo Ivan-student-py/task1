bbb3761ae59b9d7a442d2cdcdaa645e64656aebe
Звонков Иван Владимирович <ip313s10@csc.sibsutis.ru>
2025-08-27 11:45:52 +0700

    Completed task 1

---
+++ task1.py
@@ -0,0 +1,4 @@
+vector1 = list(map(float, input().split()))
+vector2 = list(map(float, input().split()))
+scalar_product = vector1[0] * vector2[0] + vector1[1] * vector2[1] + vector1[2] * vector2[2]
+print(scalar_product)
