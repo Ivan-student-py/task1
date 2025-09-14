03b651494ad40570c3893665733bc94afdb921e7
Звонков Иван Владимирович <ip313s10@csc.sibsutis.ru>
2025-09-14 23:24:46 +0700

    code for the second day

--- 01/src/exercise1/code.py
+++ 01/src/exercise1/code.py
@@ -1,7 +1,7 @@
-import asyncore
 import time
 import random
 import os
+import threading
 
 #Глобальные переменные
 golden_sech = 1.618
@@ -13,6 +13,7 @@ break_time_max = 18
 mood_bad = 1/8
 mood_good = 1/4
 mood_neit = 5/8
+
 
 #Классы
 class Student():
@@ -37,3 +38,101 @@ class Student():
         else:
             weights[0] = remainder
         return random.choices(words, weights=weights, k=1)[0]
+    
+class Examiner(threading.Thread):
+    def __init__(self, name, gender, lock, student_queue, exam_start_time):
+        self.name = name
+        self.gender = gender
+        self.student_queue = student_queue
+        self.lock = lock 
+
+        self.current_student = None
+        self.total_students = 0
+        self.failed_count = 0
+        self.work_time = 0.0
+        self.has_taken_break = False
+
+        self.exam_start_time = exam_start_time
+
+        super().__init__()
+        self.daemon = False
+
+    def run(self): #тело потока экзаменатора
+        while True:
+            student = None
+            with self.lock:
+                if self.student_queue:
+                    student = self.student_queue.pop(0)
+            if student is None:
+                break
+            
+            self.current_student = student
+            self.conduct_exam(student)
+            self.current_student = None
+
+            if not self.has_taken_break and time.time() - self.exam_start_time > when_can_be_break:
+                self.has_taken_break = True
+                time.sleep(random.uniform(break_time_min, break_time_max))
+
+    def conduct_exam(self, student): #проведение экзамена
+        questions = get_questions_from_bank(count = 3)
+
+        #класс с банком вопросов будет прописан позже
+
+        correct_answers = 0
+        total_questions = len(questions)
+        duration = random.uniform(len(self.name) - 1, len(self.name) + 1)
+
+        for question in questions:
+            correct_words = self.generate_correct_answers(question)
+            student_answer = student.answer_question(question)
+            if student_answer in correct_words:
+                correct_answers += 1
+
+        mood_roll = random.random()
+        if mood_roll < mood_bad:
+            passed = False
+        elif mood_roll < mood_bad + mood_good:
+            passed = True
+        else:
+            passed = correct_answers > (total_questions - correct_answers)
+
+        self.total_students += 1
+        if not passed:
+            self.failed_count += 1
+        self.work_time += duration
+
+        student_status[student.name] = 'Passed' if passed else 'Failed'
+        time.sleep(duration)
+
+    def get_golden_weights(self,words):
+        n = len(words)
+        weights = [0] * n
+        remainder = 1.0
+        for i in range(n - 1):
+            prob = remainder / golden_sech
+            if self.gender == "M":
+                weights[i] = prob
+            else:
+                weights[n - 1 -i] = prob
+            remainder -= prob
+        if self.gender == "M":
+            weights[n - 1] = remainder
+        else:
+            weights[0] = remainder
+        return weights
+
+    def generate_correct_answers(self, question):
+        words = question.strip().split()
+        chosen_word = random.choices(words, weights=self.get_golden_weights(words))[0]
+        correct_set = {chosen_word}
+
+        remaining_words = [w for w in words if w not in correct_set]
+        while remaining_words and random.random() < 1/3:
+            next_word = random.choice(remaining_words)
+            correct_set.add(next_word)
+            remaining_words.remove(next_word)
+
+        return correct_set
+    
+class QuestionBank
